c289a6ab565e63656773fbf979fbf80ff6c10d14
Aleksander Chernov <aleksanderancher@gmail.com>
2021-06-28 20:11:12 +0300

    функционал блокирования кнопок 1/3

--- movies-explorer-frontend/src/components/Profile/Profile.js
+++ movies-explorer-frontend/src/components/Profile/Profile.js
@@ -1,9 +1,12 @@
 import './Profile.css';
 import { useHistory } from 'react-router-dom';
-import {CurrentUserContext} from '../../contexts/CurrentUserContext.js';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext.js';
 import InfoTooltip from '../InfoTooltip/InfoTooltip.js';
 import Preloader from '../../components/Movies/Preloader/Preloader';
+import '../AuthForm/AuthForm.css'
 import React from 'react';
+import { nameRegex, emailRegex } from '../../utils/FormValidation';
+
 
 export default function Profile(props) {
 
@@ -13,6 +16,39 @@ export default function Profile(props) {
   const [name, setName] = React.useState('');
   const [message, setMessage] = React.useState('');
   const [success, setSuccess] = React.useState(false);
+
+  const [hasErrorName, setHasErrorName] = React.useState(false)
+  const [hasErrorMail, setHasErrorMail] = React.useState(false)
+  const [errorNameMessage, setErrorNameMessage] = React.useState('')
+  const [errorMailMessage, setErrorMailMessage] = React.useState('')
+  const [nameInput, setNameInput] = React.useState('')
+  const [mailInput, setMailInput] = React.useState('')
+
+  function validateEmail(event) {
+    const newMail = event.target.value;
+    setMailInput(newMail)
+    const isValid = emailRegex.test(newMail)
+    if (isValid) {
+      setHasErrorMail(false)
+      setErrorMailMessage('')
+    } else {
+      setHasErrorMail(true)
+      setErrorMailMessage('E-mail должен быть валиден')
+    }
+  }
+
+  function validateName(event) {
+    const newName = event.target.value;
+    setNameInput(newName)
+    const isValid = nameRegex.test(newName)
+    if (isValid) {
+      setHasErrorName(false)
+      setErrorNameMessage('')
+    } else {
+      setHasErrorName(true)
+      setErrorNameMessage('Имя должно быть заполнено и не иметь особых знаков/цифр')
+    }
+  }
 
   function makePopupVisible(state) {
     setInfoTooltip(state)
@@ -64,8 +100,10 @@ export default function Profile(props) {
             id="username" 
             name="username"  
             type="username"
-            onChange={handleNameChange}
-            placeholder={currentUser.name}/>
+            value={nameInput}
+            onChange={e =>{ validateName(e); handleNameChange(e)}}
+            placeholder={currentUser.name}
+          />
         </div>
         <hr className="profile__section-separator"/>
         <div className="profile__info-section">
@@ -74,10 +112,14 @@ export default function Profile(props) {
             id="email" 
             name="email"  
             type="email"
-            onChange={handleEmailChange}
-            placeholder={currentUser.email}/>
+            value={mailInput}
+            onChange={e =>{ validateEmail(e); handleEmailChange(e)}}
+            placeholder={currentUser.email}
+          />
         </div>
-        <button className="profile__button profile__button_redact" type="submit">Редактировать</button>
+        { hasErrorName && <p className="profile__error">{errorNameMessage}</p>}
+        { hasErrorMail && <p className="profile__error">{errorMailMessage}</p>}
+        <button className={`profile__button profile__button_redact ${hasErrorName || hasErrorMail ? 'profile__button_locked' : ''}`} type={ hasErrorName === false && hasErrorMail === false && "submit"}>Редактировать</button>
         <button className="profile__button profile__button_logout" onClick={onSignOut}>Выйти из аккаунта</button>
       </form>
       <InfoTooltip message={message} isOpen={isInfoTooltipOpen} isSuccessful={success}/>
